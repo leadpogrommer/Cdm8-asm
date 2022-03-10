28fd8083ec5b13c4ee8a40fa219f5af62bb6fe9b
leadpogrommer <ilyamerzlakov@gmail.com>
2022-03-10 15:31:08 +0700

    crc now works

---
+++ crc_test.asm
@@ -0,0 +1,46 @@
+asect 0x00
+jsr first:
+
+
+
+
+asect 0x1234
+first:
+jsr second
+inc r1
+crc
+inc r1
+crc
+inc r1
+crc
+inc r1
+crc
+inc r1
+crc
+inc r1
+crc
+inc r1
+crc
+halt
+
+
+
+
+asect 0x5678
+second:
+inc r2
+crc
+inc r2
+crc
+inc r2
+crc
+inc r2
+crc
+inc r2
+crc
+inc r2
+crc
+halt
+
+
+end
